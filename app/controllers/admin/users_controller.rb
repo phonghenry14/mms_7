dcdee55e2bd642722535d2469c55f3712c7e3716
minhld1709 <minhld1709@gmail.com>
2015-06-10 11:04:05 +0700

    admin_show_index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,10 @@ class Admin::UsersController < ApplicationController
     @users = User.normal.paginate page: params[:page]
   end
 
+  def show
+    @user = User.find params[:id]
+  end
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = t('skill.destroy.success')
