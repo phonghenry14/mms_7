a572cddc01bb46b46ddda757b6fba23895378ad9
phonghenry14 <phonghenry14@gmail.com>
2015-06-17 09:53:28 +0700

    add level and year to skill

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,23 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  scope :normal, ->{where role: Settings.user.role.normal}
-  scope :no_team, ->{where team_id: nil}
-
-  validates :name, presence: true, uniqueness: true, length: {maximum: Settings.user.name.maximum}
-  validates :email, presence: true, length: {maximum: Settings.user.email.maximum}
-
-  has_many :userskills
+  has_many :userskills, dependent: :destroy
   has_many :skills, through: :userskills
-  has_many :user_positions
+  has_many :user_positions, dependent: :destroy
   has_many :positions, through: :user_positions
 
   accepts_nested_attributes_for :userskills, allow_destroy: true
   accepts_nested_attributes_for :user_positions, allow_destroy: true
 
+  validates :name, presence: true, uniqueness: true, length: {maximum: Settings.user.name.maximum}
+  validates :email, presence: true, length: {maximum: Settings.user.email.maximum}
+
   after_create :log_create
   after_update :log_update
   after_destroy :log_destroy
+
+  scope :normal, ->{where role: Settings.user.role.normal}
+  scope :no_team, ->{where team_id: nil}
 
   def is_admin?
     role == Settings.user.role.admin
--- app/models/userskill.rb
+++ app/models/userskill.rb
@@ -1,4 +1,6 @@
 class Userskill < ActiveRecord::Base
+  enum level: [:Beginner, :Amateur, :Professional, :ToCoder, :SuperStar, :Legendary]
+
   belongs_to :user
   belongs_to :skill
 end
