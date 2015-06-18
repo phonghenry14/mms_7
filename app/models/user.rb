6444f6ffe82ca4bb4756110799897bef731a0519
minhld1709 <minhld1709@gmail.com>
2015-06-18 14:19:27 +0700

    user project

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :user_projects, dependent: :destroy
+  has_many :projects, through: :user_projects
+
   has_many :userskills, dependent: :destroy
   has_many :skills, through: :userskills
   has_many :user_positions, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616023432) do
+ActiveRecord::Schema.define(version: 20150618071011) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "action"
@@ -61,6 +61,13 @@ ActiveRecord::Schema.define(version: 20150616023432) do
 
   add_index "user_positions", ["position_id"], name: "index_user_positions_on_position_id"
   add_index "user_positions", ["user_id"], name: "index_user_positions_on_user_id"
+
+  create_table "user_projects", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name",                   default: "",       null: false
